3f0b5ca3a23ed18d52fb6e28ba9f1ca89e0d0c80
theilorsilva <martins.theilor8@gmail.com>
2025-01-06 11:42:12 -0300

    Criado acesso ao banco usando Django ORM

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+
+    nome = models.CharField('Nome', max_length=100, null=False, blank=False)
+    legenda = models.CharField('Legenda', max_length=150, null=False, blank=False)
+    descricao = models.TextField('Descricao', null=False, blank=False)
+    foto= models.CharField('Foto', max_length=150, null=False, blank=False)
+
+    def __str__(self):
+        return f'Fotografia: [nome = {self.nome}'
+
