550e1aa9545cc239820f3d57ba74ff816245bfa5
theilorsilva <martins.theilor8@gmail.com>
2025-01-07 07:01:43 -0300

    Criado mecanismo de busca, filtros e upload de imagens

--- galeria/models.py
+++ galeria/models.py
@@ -1,12 +1,24 @@
 from django.db import models
+from datetime import datetime
 
 class Fotografia(models.Model):
 
+    OPCOES_CATEGORIA = [
+        ('NEBULOSA','Nebulosa'),
+        ('ENTRELA','Estrela'),
+        ('GALÁXIA','Galáxia'),
+        ('PLANETA','Planeta'),
+    ]
+
     nome = models.CharField('Nome', max_length=100, null=False, blank=False)
     legenda = models.CharField('Legenda', max_length=150, null=False, blank=False)
+    categoria = models.CharField('Categoria', choices = OPCOES_CATEGORIA, max_length=100, default = '')
     descricao = models.TextField('Descricao', null=False, blank=False)
-    foto= models.CharField('Foto', max_length=150, null=False, blank=False)
+    foto= models.ImageField(upload_to='fotos/%Y/%m/%d', blank=True)
+    publicada = models.BooleanField('Publicada', default=False)
+    data_fotografia = models.DateTimeField('Data de Fotografia', default=datetime.now, blank=False)
+
 
     def __str__(self):
-        return f'Fotografia: [nome = {self.nome}'
+        return self.nome
 
--- setup/settings.py
+++ setup/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+
+from django.conf.global_settings import MEDIA_ROOT
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -127,6 +129,10 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
